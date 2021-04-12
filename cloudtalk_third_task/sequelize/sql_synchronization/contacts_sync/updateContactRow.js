3e15590f76c368042798f181dc725005461902ae
Juraj Zovinec <juraj.zovinecc@gmail.com>
2021-04-12 04:18:46 +0200

    Remove: commented out lines.

--- cloudtalk_third_task/sequelize/sql_synchronization/contacts_sync/updateContactRow.js
+++ cloudtalk_third_task/sequelize/sql_synchronization/contacts_sync/updateContactRow.js
@@ -14,7 +14,6 @@ const updateContactRow = async (contactObject) => {
     
     selectedContact.update(ticketObjectEditForSQL).catch(error=>{throw error;});
 
-
 };
 
 module.exports = updateContactRow;
